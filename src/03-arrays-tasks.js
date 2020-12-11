db3e8689b7782ec22199c52b8a1b73a14aef73c8
Panfilenka Natallia <choco-cat@mail.ru>
2020-12-11 12:38:46 +0300

    feat: 50 tsk

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -269,13 +269,14 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 
-function propagateItemsByPositionIndex(arr) {
-  let result = [];
+function propagateItemsByPositionIndex(/* arr */) {
+  throw new Error('Not implemented');
+  /* let result = [];
   arr.reduce((newArr, el, index) => {
     result = [...newArr, ...Array.from({ length: index + 1 }).fill(el)];
     return newArr;
   }, result);
-  return result;
+  return result; */
 }
 
 /**
